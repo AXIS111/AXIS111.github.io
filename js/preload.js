e47e46755e5c3a1c9d8b24949b5f583c096abcc1
AXIS111 <39508686+AXIS111@users.noreply.github.com>
2018-12-15 22:22:46 +0800

    Update preload.js

--- js/preload.js
+++ js/preload.js
@@ -11,7 +11,7 @@ document.onreadystatechange = completeLoading;
  
 // 加载状态为complete时移除loading效果
 function completeLoading() {
-　　if (document.readyState == "complete") {
+　　if (document.readyState == "loading") {
 　　　　var loadingMask = document.getElementById('loadingDiv');
 　　　　loadingMask.parentNode.removeChild(loadingMask);
 　　}
