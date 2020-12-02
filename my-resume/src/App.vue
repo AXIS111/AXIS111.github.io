6c220d6ffe89567b431fdcd67aac2489049a9da8
axispc <18684847366@163.com>
2020-12-03 07:10:17 +0800

    Signed-off-by: axispc <18684847366@163.com>

--- my-resume/src/App.vue
+++ my-resume/src/App.vue
@@ -11,25 +11,25 @@
           width="40"
         />
 
-        <v-img
+        <!-- <v-img
           alt="Vuetify Name"
           class="shrink mt-1 hidden-sm-and-down"
           contain
           min-width="100"
           src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
           width="100"
-        />
+        /> -->
       </div>
 
       <v-spacer></v-spacer>
 
       <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        href="#"
         target="_blank"
         text
       >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
+        <span class="mr-2">axis's resume</span>
+        <!-- <v-icon>mdi-open-in-new</v-icon> -->
       </v-btn>
     </v-app-bar>
 
