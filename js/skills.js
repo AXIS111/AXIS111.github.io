7cb4dd58a3100f34cc8b69b4a21bbf2343baeca8
axispc <18684847366@163.com>
2020-12-03 05:33:23 +0800

    Signed-off-by: axispc <18684847366@163.com>

--- js/skills.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-		$("#checkskill").click(function () {
-			$('.skillbar').each(function(){
-			$(this).find('.skillbar-bar').animate({width:$(this).attr('data-percent')
-		},5000);
-	});
-		$(".skill-bar-percent").show(5000);
-	})
-});
