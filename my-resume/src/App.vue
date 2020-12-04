f2b010d0dacb753c2958122009d1865af588a37c
axispc <18684847366@163.com>
2020-12-05 07:13:49 +0800

    Signed-off-by: axispc <18684847366@163.com>

---
+++ my-resume/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+    <div id="app">
+        <home></home>
+    </div>
+</template>
+
+<style lang="less">
+* {
+    margin: 0;
+    padding: 0;
+}
+
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+}
+
+#nav {
+    padding: 2rem;
+}
+</style>
+
+<script>
+import home from "../src/views/Home.vue";
+export default {
+    name: "APP",
+    components: {
+        home,
+    },
+    created() {
+        const htmlwidth =
+            document.documentElement.clientWidth || document.body.clientWidth;
+        const htmlDom = document.getElementsByTagName("html")[0];
+        htmlDom.style.fontSize = htmlwidth / 20 + "px";
+    },
+};
+</script>
---
+++ my-resume/src/views/Home.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <mt-header title="">
+            <router-link to="/" slot="left">
+                <mt-button>
+                    <img
+                        src="../assets/list.png"
+                        height="20"
+                        width="20"
+                        slot="icon"
+                    />About
+                </mt-button>
+            </router-link>
+            <router-link to="/" slot="right">
+                <mt-button >
+                    <img
+                        src="../assets/phonecall.png"
+                        height="20"
+                        width="20"
+                        slot="icon"
+                    />Contact
+                </mt-button>
+            </router-link>
+        </mt-header>
+        <div>1</div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Home",
+};
+</script>
+
+<style lang="less" scoped>
+</style>
